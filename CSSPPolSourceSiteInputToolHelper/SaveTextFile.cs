3e7d873f3cf08951c10738528ae3b963fbb737aa
ECCC-CSSP <charles.leblanc2@canada.ca>
2018-06-21 11:53:27 -0300

    Init

--- CSSPPolSourceSiteInputToolHelper/SaveTextFile.cs
+++ CSSPPolSourceSiteInputToolHelper/SaveTextFile.cs
@@ -559,7 +559,10 @@ namespace CSSPPolSourceSiteInputToolHelper
                     {
                         sb.AppendLine($"ISSUETOREMOVE\t");
                     }
-                    sb.AppendLine($"EXTRACOMMENT\t{issue.ExtraComment.Replace("\r\n", "|||").Replace("\t", "    ")}\t");
+                    if (issue.ExtraComment != null)
+                    {
+                        sb.AppendLine($"EXTRACOMMENT\t{issue.ExtraComment.Replace("\r\n", "|||").Replace("\t", "    ")}\t");
+                    }
                     if (issue.ExtraCommentNew != null)
                     {
                         sb.AppendLine($"EXTRACOMMENTNEW\t{issue.ExtraCommentNew.Replace("\r\n", "|||").Replace("\t", "    ")}\t");
