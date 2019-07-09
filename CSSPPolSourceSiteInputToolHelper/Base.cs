0fce58d6d101d0f2858e376a1cd8aba8855d92ca
ECCC-CSSP <charles.leblanc2@canada.ca>
2019-07-09 08:42:51 -0300

    Init

--- CSSPPolSourceSiteInputToolHelper/Base.cs
+++ CSSPPolSourceSiteInputToolHelper/Base.cs
@@ -8,8 +8,6 @@ using System.Collections.Specialized;
 using System.Drawing;
 using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace CSSPPolSourceSiteInputToolHelper
